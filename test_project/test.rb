b2dab895f1ccbf8f80809d7995196697cea5e1a1
mocchi0420 <mocchi.0420@gmail.com>
2016-05-01 04:32:18 +0900

    ファイルを分割

--- test_project/test.rb
+++ test_project/test.rb
@@ -9,99 +9,6 @@ require 'pp'
 #フォントの設定
 font = Font.new(24)  # 第２引数を省略するとＭＳ Pゴシックになります
 
-
-# ********************************
-# ==== ファイル読み書き系機能 ====
-# ********************************
-
-#ファイル開いたり書き込んだりする系の処理を全部やらせるためのモジュール
-module FileManager
-	module_function
-	def load_csv(file_name)
-		Encoding.default_external = 'UTF-8' #デフォルトがWindows31-Jなのでエンコーディング設定は必須になるので注意！！
-		ret = CSV.table("./data/#{file_name}").each_with_object({}) do |data, my_hash|
-			my_hash[data[:id]] = data.to_hash
-			my_hash[data[:id]][:graph] = my_hash[data[:id]][:graph].to_sym
-		end
-		return ret
-	end
-	
-	def save_json(file_name, my_hash={})
-		Encoding.default_external = 'UTF-8' #デフォルトがWindows31-Jなのでエンコーディング設定は必須になるので注意！！
-		File.open("./save/#{file_name}", "w") do |file|
-  			file.puts(my_hash.to_json.to_s)
-		end
-	end
-	
-	def load_json(file_name)
-		Encoding.default_external = 'UTF-8' #デフォルトがWindows31-Jなのでエンコーディング設定は必須になるので注意！！
-		tmp = File.open("./save/#{file_name}") do |io|
-  			JSON.load(io)
-		end
-		return tmp
-	end
-	
-	def save_8byte_seq(file_name, my_str="")
-		File.open("./save/#{file_name}", "w") do |file|
-  			file.puts(my_str.to_s)
-		end
-	end
-
-	def load_8byte_seq(file_name)
-		File.read("./save/#{file_name}").force_encoding("ASCII-8BIT").chomp
-	end
-end
-
-#暗号化・復号化をやらせるためのモジュール
-module MyCipher
-	require 'openssl'
-	include FileManager
-	
-	Cipher_type = "AES-256-CBC"
-
-	module_function
-	def encrypt_data(data, password, salt, cipher_conf=Cipher_type)
-  		cipher = OpenSSL::Cipher::Cipher.new(cipher_conf)
-  		cipher.encrypt
-  		cipher.pkcs5_keyivgen(password, salt)
-  		cipher.update(data) + cipher.final
-	end
-	
-	def decrypt_data(data, password, salt, cipher_conf=Cipher_type)
-  		cipher = OpenSSL::Cipher::Cipher.new(cipher_conf)
-  		cipher.decrypt
-  		cipher.pkcs5_keyivgen(password, salt)
-  		cipher.update(data) + cipher.final
-	end
-	
-	def get_salt
-		OpenSSL::Random.random_bytes(8)
-	end
-
-	#データを直接暗号化して外部ファイルに書き出すよ
-	def save_as_encrypted(data, file_name)
-		salt = self.get_salt
-		pass = Digest::SHA1.hexdigest(salt)
-		if data.kind_of?(Hash) == true
-			tmp_data = data.to_json
-		else
-			tmp_data = data
-		end
-		save_data = self.encrypt_data(tmp_data, pass ,salt)
-		set_strings = "Salted__"+salt+"__"+save_data
-		FileManager.save_8byte_seq(file_name, set_strings)
-	end
-	
-	#ファイルから直接データを復号してハッシュとして返すよ
-	def load_as_decrypted(file_name)
-		tmp = FileManager.load_8byte_seq(file_name).split("__")
-		pass = Digest::SHA1.hexdigest(tmp[1])
-		JSON.parse(self.decrypt_data(tmp[2], pass , tmp[1]))	
-	end
-
-end
-
-
 # ************************************************
 # ==== ゲーム内で使用するオブジェクト用の機能 ====
 # ************************************************
@@ -224,12 +131,15 @@ end
 #end
 
 class Idle
+	require './FileManager.rb'
 	include Singleton
 	include FileManager
 	attr_reader :idle
 	
 	def initialize
 		@idle = FileManager.load_csv('test.csv')
+		#hoge = FileManager.load_json("test.txt")
+		#MyCipher.save_as_encrypted(hoge.to_json, "hoge.txt")
 	end
 	
 	def self.idle
@@ -243,8 +153,6 @@ class Idle
 end
 
 
-hoge = FileManager.load_json("test.txt")
-MyCipher.save_as_encrypted(hoge.to_json, "hoge.txt")
 chara_a = Idle.generate(10001)
 chara_b = Idle.generate(10002)
 
